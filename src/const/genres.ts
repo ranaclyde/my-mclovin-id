2bc68cfa60e4b81042215dd78122338309ac1dc5
Matías E Sanhueza <manoloesanhueza@gmail.com>
2024-03-30 19:35:25 -0300

    Create const folder

---
+++ src/const/genres.ts
@@ -0,0 +1,7 @@
+import type { SelectOptions } from '@/types'
+
+export const genresOptions: SelectOptions[] = [
+  { value: 'F', text: 'Female' },
+  { value: 'M', text: 'Male' },
+  { value: 'O', text: 'Other' },
+]
---
+++ src/const/mclovinData.ts
@@ -0,0 +1,32 @@
+export const mclovin = {
+  name: {
+    value: 'McLOVIN',
+    posX: 30,
+    posY: 820,
+  },
+  dob: {
+    value: '06/03/1981',
+    posX: 700,
+    posY: 364,
+  },
+  exp: {
+    value: '06/03/2008',
+    posX: 1160,
+    posY: 364,
+  },
+  sex: {
+    value: 'M',
+    posX: 1244,
+    posY: 477,
+  },
+  issueDate: {
+    value: '06/18/1998',
+    posX: 592,
+    posY: 610,
+  },
+}
+
+export const fontSize = {
+  title: 65,
+  subtitle: 36,
+}
