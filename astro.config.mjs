a003ad32828144e76b385fb7ef63266f9cb12bb7
Matías E Sanhueza <manoloesanhueza@gmail.com>
2026-04-24 17:52:34 -0300

    feat: add sitemap integration, update layout with Open Graph and structured data, and create robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://mymclovinid.netlify.app',
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
